0fdc5a47c92b8ec90d545895bc2d83f7e521c1e7
Vetosy <xigga11@gmail.com>
2022-12-19 23:11:00 +0400

    added top-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
-$(function () {})
+$(function () {
+  $('.top-slider__content').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+  })
+})
